d915607a3f9c4809abf60c010959f1da0af3825e
Lars Engblom <lars.engblom@kimitotelefon.fi>
2016-01-02 22:37:59 +0200

    Remove dependency from project.clj. Bump version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject gpio "0.1.0-SNAPSHOT"
+(defproject gpio "0.1.0"
   :description "A simple library for using the GPIO headers on Raspberry Pi computers"
   :url "https://github.com/engblom/gpio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clojure-watch "LATEST"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
