c04ec4c59f20bae6c4e88f7a8889a5792f4e1c3d
Lars Engblom <lars.engblom@kimitotelefon.fi>
2019-10-22 20:50:15 +0100

    Bumped Clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gpio "0.2.2"
+(defproject gpio "0.2.3"
   :description "A simple library for using the GPIO headers on Raspberry Pi computers"
   :url "https://github.com/engblom/gpio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
